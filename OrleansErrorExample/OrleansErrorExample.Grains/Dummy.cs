e7067d49aaa8764301712ca008dd8950104a9c28
Mark Watts <mark.watts@hotmail.co.uk>
2017-02-27 13:59:53 +0000

    Initial addition of example source code

---
+++ OrleansErrorExample/OrleansErrorExample.Grains/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+using OrleansErrorExample.Interfaces;
+
+namespace OrleansErrorExample.Grains {
+    public class Dummy : Grain, IDummy {
+    }
+}
