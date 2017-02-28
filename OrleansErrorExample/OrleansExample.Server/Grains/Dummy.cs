a7ad623775074e553b15c9b771c2e346f1430598
Mark Watts <mark.watts@hotmail.co.uk>
2017-02-28 09:02:46 +0000

    Add all-in-one example

---
+++ OrleansErrorExample/OrleansExample.Server/Grains/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+using OrleansExample.Server.Interfaces;
+
+namespace OrleansExample.Server.Grains {
+    public class Dummy : Grain, IDummy {
+    }
+}
---
+++ OrleansErrorExample/OrleansExample.Server/Interfaces/IDummy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace OrleansExample.Server.Interfaces {
+    public interface IDummy : IGrain {
+    }
+}
